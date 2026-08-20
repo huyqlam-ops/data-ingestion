8ada9f863fa538f20dc7db88725cf8bc1df8d22b
Lam Quoc Huy <huyqlam@kms-technology.com>
2026-08-20 11:23:22 +0700

    fix build

--- src/test/java/com/cloudnative/dataingest/DataingestApplicationTests.java
+++ src/test/java/com/cloudnative/dataingest/DataingestApplicationTests.java
@@ -4,9 +4,11 @@ import com.azure.messaging.eventhubs.EventProcessorClient;
 import com.cloudnative.dataingest.data.ReportRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DataingestApplicationTests {
 
 	@MockitoBean
